08b939255e5cb7ba8234a491a07e14fad7037b2f
Troy Thompson <troycthompson@gmail.com>
2014-07-21 14:27:56 -0700

    1st attempt to fix sinatra error

--- demo/sms.rb
+++ demo/sms.rb
@@ -3,8 +3,11 @@ require 'twilio-ruby'
 require 'sinatra'
 
 get '/sms' do
+
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey there!. Thanks for the message!"
+    r.Message "Hey Monkey. Thanks for the message!"
   end
+
   twiml.text
+
 end
